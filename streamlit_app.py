2563f798ddd5bfbc913f61af52c28ab18e527588
Abiodun-34 <1abiodun34@gmail.com>
2024-07-18 05:15:52 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,27 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("Welcome To Healthnics")
+
+about_page = st.Page(
+    page="views/Welcome.py",
+    title="Welcome",
+    icon=":material/account_circle:",
+    default=True,
 )
+project_1_page = st.Page(
+    page="views/Vitals.py",
+    title="Vitals",
+    icon=":material/thumb_up:",
+)
+project_2_page = st.Page(
+    page="views/Goodbye.py",
+    title="Goodbye",
+    icon=":material/smart_toy:",
+)
+
+pg = st.navigation(pages=[about_page, project_1_page, project_2_page])
+
+pg.run()
+
+
+
